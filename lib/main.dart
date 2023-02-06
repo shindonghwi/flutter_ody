8aa64b0c38f6e7be48ffa9bbe9f886aed15299fc
shindonghwi <ehdgnl8940@naver.com>
2023-02-06 18:37:08 +0900

    feat: localization 적용 ( 영어, 한국어 대음 )

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:odac_flutter_app/screens/home/HomeScreen.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]); // screen 세로 고정
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,6 +24,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      supportedLocales: AppLocalizations.supportedLocales,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
       home: const HomeScreen(),
     );
   }
