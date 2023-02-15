b2770e0c9343a38c827f2caa09499f11a8c34c23
shindonghwi <ehdgnl8940@naver.com>
2023-02-15 16:52:38 +0900

    feat: add Riverpod pod

--- lib/app/feature/home/HomeScreen.dart
+++ lib/app/feature/home/HomeScreen.dart
@@ -1,29 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:odac_flutter_app/app/feature/home/provider/LoginProvider.dart';
-import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-        // ChangeNotifierProvider를 통해 변화에 대해 구독(하나만 구독 가능)
-        create: (BuildContext context) => LoginProvider(),
-        // count_provider.dart
-        child:
-            HomeContent() // home.dart // child 하위에 모든 것들은 CountProvider에 접근 할 수 있다.
-        );
+    return HomeContent();
   }
 }
 
 class HomeContent extends StatelessWidget {
-  late LoginProvider _loginProvider;
-
   @override
   Widget build(BuildContext context) {
     // Provider를 호출해 접근, listen: false를 통해 구독된 모든 위젯들에게 알림을 보내지 않게 한다.
-    _loginProvider = Provider.of<LoginProvider>(context, listen: false);
 
     return Scaffold(
       appBar: AppBar(
@@ -36,13 +25,13 @@ class HomeContent extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.add),
             onPressed: () {
-              _loginProvider.add(); // 클릭 되었을 때 add()
+              // _loginProvider.add(); // 클릭 되었을 때 add()
             },
           ),
           IconButton(
             icon: Icon(Icons.remove),
             onPressed: () {
-              _loginProvider.remove(); // 클릭 되었을 때 remove()
+              // _loginProvider.remove(); // 클릭 되었을 때 remove()
             },
           )
         ],
@@ -57,14 +46,7 @@ class CountHomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child:
-          Consumer<LoginProvider>(// Consumer를 활용해서 provider에 접근하여 데이터를 받아올 수 있다
-              builder: (context, provider, child) {
-        return Text(
-          provider.count.toString(), // count를 화면에 출력
-          style: TextStyle(fontSize: 40.0),
-        );
-      }),
+      child: Text("Hi"),
     );
   }
 }
--- lib/env/Environment.dart
+++ lib/env/Environment.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:odac_flutter_app/app/OrotApp.dart';
 
 enum BuildType { dev, prod }
@@ -22,9 +23,8 @@ class Environment {
 
   static BuildType get buildType => _instance._buildType;
 
-  static String get apiUrl => _instance._buildType == BuildType.dev
-      ? 'https://dev url'
-      : 'https://prod url'; // api 주소
+  static String get apiUrl =>
+      _instance._buildType == BuildType.dev ? 'https://dev url' : 'https://prod url'; // api 주소
 
   factory Environment.newInstance(BuildType buildType) {
     _instance = Environment._internal(buildType);
@@ -34,6 +34,6 @@ class Environment {
   bool get isDebuggable => _buildType == BuildType.dev;
 
   void run() {
-    runApp(OrotApp());
+    runApp(ProviderScope(child: OrotApp()));
   }
 }
