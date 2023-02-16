5153062b19e992307c0f6951837e1704e27f5481
shindonghwi <ehdgnl8940@naver.com>
2023-02-16 12:42:30 +0900

    refactor: 주석추가 (KeepAlivePage)

--- lib/app/feature/components/common/KeepAlivePage.dart
+++ lib/app/feature/components/common/KeepAlivePage.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 /**
  * @feature: PageView를 사용하면서 페이지의 상태를 유지하기 위한 위젯이다.
  * @author: 2023/02/14 4:03 PM donghwishin
-*/
+ *
+ * @exampel:{
+ *  PageView(child: Text("Hello World"));
+ * }
+ */
 class KeepAlivePage extends StatefulWidget {
   KeepAlivePage({Key? key, required this.child}) : super(key: key);
 
