d5ce8aa87ca425a6122a1f072362556072807c12
shindonghwi <ehdgnl8940@naver.com>
2023-03-28 13:10:42 +0900

    style: 오타 수정

--- lib/presentation/ui/typography.dart
+++ lib/presentation/ui/typography.dart
@@ -52,19 +52,19 @@ final title3 = defaultTextStyle.copyWith(
   height: 1.5
 );
 
-final lable1 = defaultTextStyle.copyWith(
+final label1 = defaultTextStyle.copyWith(
   fontSize: 18,
   fontWeight: FontWeight.w500,
   height: 1.33
 );
 
-final lable2 = defaultTextStyle.copyWith(
+final label2 = defaultTextStyle.copyWith(
   fontSize: 16,
   fontWeight: FontWeight.w500,
   height: 1.5
 );
 
-final lable3 = defaultTextStyle.copyWith(
+final label3 = defaultTextStyle.copyWith(
   fontSize: 14,
   fontWeight: FontWeight.w500,
   height: 1.42
@@ -104,77 +104,4 @@ final caption3 = defaultTextStyle.copyWith(
   fontSize: 10,
   fontWeight: FontWeight.w400,
   height: 1.6
-);
-
-
-// Text Theme
-TextTheme odacTextTheme = TextTheme(
-  displayLarge: defaultTextStyle.copyWith(
-    fontSize: 57,
-    height: 1.12,
-  ),
-  displayMedium: defaultTextStyle.copyWith(
-    fontSize: 45,
-    height: 1.15,
-  ),
-  displaySmall: defaultTextStyle.copyWith(
-    fontSize: 36,
-    height: 1.22,
-  ),
-  headlineLarge:defaultTextStyle.copyWith(
-    fontSize: 30,
-    height: 1.25,
-  ),
-  headlineMedium: defaultTextStyle.copyWith(
-    fontSize: 26,
-    height: 1.28,
-  ),
-  headlineSmall: defaultTextStyle.copyWith(
-    fontSize: 22,
-    height: 1.33,
-  ),
-  titleLarge: defaultTextStyle.copyWith(
-    fontSize: 20,
-    height: 1.27,
-  ),
-  titleMedium: defaultTextStyle.copyWith(
-    fontSize: 16,
-    height: 1.5,
-    letterSpacing: 0.15,
-  ),
-  titleSmall: defaultTextStyle.copyWith(
-    fontSize: 14,
-    height: 1.42,
-    letterSpacing: 0.1,
-  ),
-  bodyLarge: defaultTextStyle.copyWith(
-    fontSize: 16,
-    height: 1.5,
-    letterSpacing: 0.5,
-  ),
-  bodyMedium: defaultTextStyle.copyWith(
-    fontSize: 14,
-    height: 1.42,
-    letterSpacing: 0.25,
-  ),
-  bodySmall: defaultTextStyle.copyWith(
-    fontSize: 12,
-    height: 1.33,
-    letterSpacing: 0.4,
-  ),
-  labelLarge: defaultTextStyle.copyWith(
-    fontSize: 14,
-    height: 1.42,
-    letterSpacing: 0.1,
-  ),
-  labelMedium: defaultTextStyle.copyWith(
-    fontSize: 12,
-    height: 1.33,
-    letterSpacing: 0.5,
-  ),
-  labelSmall: defaultTextStyle.copyWith(
-    fontSize: 8,
-    height: 145,
-    letterSpacing: 0.5,
-  ),
 );
