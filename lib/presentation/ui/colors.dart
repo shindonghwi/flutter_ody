56642c75866499155654f89a6e9147d2cd112d7a
shindonghwi <ehdgnl8940@naver.com>
2023-03-28 19:09:56 +0900

    feat: color 디자인 시스템 반영

--- lib/presentation/ui/colors.dart
+++ lib/presentation/ui/colors.dart
@@ -2,5 +2,45 @@ import 'package:flutter/material.dart';
 
 
 extension StyleColor on ColorScheme{
-  Color get primary100 => brightness == Brightness.light ? Colors.green : Colors.orange;
+
+  Color get colorPrimary => primary40;
+  Color get colorOnPrimary => primary100;
+  Color get colorError => error40;
+  Color get colorOnError => error100;
+
+  Color get primary100 => brightness == Brightness.light ? Color(0xFFFFFFFF) : Color(0xFFFFFFFF);
+  Color get primary90 => brightness == Brightness.light ? Color(0xFFECF2FF) : Color(0xFFECF2FF);
+  Color get primary80 => brightness == Brightness.light ? Color(0xFFD9E5FF) : Color(0xFFD9E5FF);
+  Color get primary70 => brightness == Brightness.light ? Color(0xFFC6D9FF) : Color(0xFFC6D9FF);
+  Color get primary60 => brightness == Brightness.light ? Color(0xFFA0BFFF) : Color(0xFFA0BFFF);
+  Color get primary50 => brightness == Brightness.light ? Color(0xFF7BA5FF) : Color(0xFF7BA5FF);
+  Color get primary40 => brightness == Brightness.light ? Color(0xFF427FFF) : Color(0xFF427FFF);
+  Color get primary30 => brightness == Brightness.light ? Color(0xFF366AD6) : Color(0xFF366AD6);
+  Color get primary20 => brightness == Brightness.light ? Color(0xFF254587) : Color(0xFF254587);
+  Color get primary10 => brightness == Brightness.light ? Color(0xFF0E1E41) : Color(0xFF0E1E41);
+  Color get primary0 => brightness == Brightness.light ? Color(0xFF000000) : Color(0xFF000000);
+
+  Color get error100 => brightness == Brightness.light ? Color(0xFFFFFFFF) : Color(0xFFFFFFFF);
+  Color get error90 => brightness == Brightness.light ? Color(0xFFFFECEC) : Color(0xFFFFECEC);
+  Color get error80 => brightness == Brightness.light ? Color(0xFFFFD9D9) : Color(0xFFFFD9D9);
+  Color get error70 => brightness == Brightness.light ? Color(0xFFFFC6C6) : Color(0xFFFFC6C6);
+  Color get error60 => brightness == Brightness.light ? Color(0xFFFF7B7B) : Color(0xFFFF7B7B);
+  Color get error50 => brightness == Brightness.light ? Color(0xFFFF6868) : Color(0xFFFF6868);
+  Color get error40 => brightness == Brightness.light ? Color(0xFFFF4242) : Color(0xFFFF4242);
+  Color get error30 => brightness == Brightness.light ? Color(0xFFD63333) : Color(0xFFD63333);
+  Color get error20 => brightness == Brightness.light ? Color(0xFF9F2929) : Color(0xFF9F2929);
+  Color get error10 => brightness == Brightness.light ? Color(0xFF551515) : Color(0xFF551515);
+  Color get error0 => brightness == Brightness.light ? Color(0xFF000000) : Color(0xFF000000);
+
+  Color get neutral100 => brightness == Brightness.light ? Color(0xFFFFFFFF) : Color(0xFFFFFFFF);
+  Color get neutral90 => brightness == Brightness.light ? Color(0xFFF6F6F6) : Color(0xFFF6F6F6);
+  Color get neutral80 => brightness == Brightness.light ? Color(0xFFEAEAEA) : Color(0xFFEAEAEA);
+  Color get neutral70 => brightness == Brightness.light ? Color(0xFFD4D4D4) : Color(0xFFD4D4D4);
+  Color get neutral60 => brightness == Brightness.light ? Color(0xFFB7B7B7) : Color(0xFFB7B7B7);
+  Color get neutral50 => brightness == Brightness.light ? Color(0xFF8E8E8E) : Color(0xFF8E8E8E);
+  Color get neutral40 => brightness == Brightness.light ? Color(0xFF666666) : Color(0xFF666666);
+  Color get neutral30 => brightness == Brightness.light ? Color(0xFF515151) : Color(0xFF515151);
+  Color get neutral20 => brightness == Brightness.light ? Color(0xFF3D3D3D) : Color(0xFF3D3D3D);
+  Color get neutral10 => brightness == Brightness.light ? Color(0xFF282828) : Color(0xFF282828);
+  Color get neutral0 => brightness == Brightness.light ? Color(0xFF000000) : Color(0xFF000000);
 }
