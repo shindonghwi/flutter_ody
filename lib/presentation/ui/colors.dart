8804da7461ba07945c835561fb2127b2a99b5e72
shindonghwi <ehdgnl8940@naver.com>
2023-03-29 11:47:28 +0900

    feat: color design system 반영

--- lib/presentation/ui/colors.dart
+++ lib/presentation/ui/colors.dart
@@ -7,8 +7,14 @@ extension StyleColor on ColorScheme{
   Color get colorOnPrimary => primary100;
   Color get colorError => error40;
   Color get colorOnError => error100;
+  Color get colorBackground => neutral10;
+  Color get colorOnBackground => neutral100;
+  Color get colorSurface => neutral100;
+  Color get colorOnSurface => neutral10;
 
   Color get primary100 => brightness == Brightness.light ? Color(0xFFFFFFFF) : Color(0xFFFFFFFF);
+  // Color get primary99 => brightness == Brightness.light ? Color(0xFFFAFBFF) : Color(0xFFFAFBFF);
+  // Color get primary95 => brightness == Brightness.light ? Color(0xFFF3F7FF) : Color(0xFFF3F7FF);
   Color get primary90 => brightness == Brightness.light ? Color(0xFFECF2FF) : Color(0xFFECF2FF);
   Color get primary80 => brightness == Brightness.light ? Color(0xFFD9E5FF) : Color(0xFFD9E5FF);
   Color get primary70 => brightness == Brightness.light ? Color(0xFFC6D9FF) : Color(0xFFC6D9FF);
@@ -21,6 +27,8 @@ extension StyleColor on ColorScheme{
   Color get primary0 => brightness == Brightness.light ? Color(0xFF000000) : Color(0xFF000000);
 
   Color get error100 => brightness == Brightness.light ? Color(0xFFFFFFFF) : Color(0xFFFFFFFF);
+  // Color get error99 => brightness == Brightness.light ? Color(0xFFFFF8F8) : Color(0xFFFFF8F8);
+  // Color get error95 => brightness == Brightness.light ? Color(0xFFFFF3F3) : Color(0xFFFFF3F3);
   Color get error90 => brightness == Brightness.light ? Color(0xFFFFECEC) : Color(0xFFFFECEC);
   Color get error80 => brightness == Brightness.light ? Color(0xFFFFD9D9) : Color(0xFFFFD9D9);
   Color get error70 => brightness == Brightness.light ? Color(0xFFFFC6C6) : Color(0xFFFFC6C6);
@@ -33,6 +41,8 @@ extension StyleColor on ColorScheme{
   Color get error0 => brightness == Brightness.light ? Color(0xFF000000) : Color(0xFF000000);
 
   Color get neutral100 => brightness == Brightness.light ? Color(0xFFFFFFFF) : Color(0xFFFFFFFF);
+  Color get neutral99 => brightness == Brightness.light ? Color(0xFFFAFAFA) : Color(0xFFFAFAFA);
+  Color get neutral95 => brightness == Brightness.light ? Color(0xFFF6F6F6) : Color(0xFFF6F6F6);
   Color get neutral90 => brightness == Brightness.light ? Color(0xFFF6F6F6) : Color(0xFFF6F6F6);
   Color get neutral80 => brightness == Brightness.light ? Color(0xFFEAEAEA) : Color(0xFFEAEAEA);
   Color get neutral70 => brightness == Brightness.light ? Color(0xFFD4D4D4) : Color(0xFFD4D4D4);
