aa9c36b96130c22edb9507867148e6747385e46c
shindonghwi <ehdgnl8940@naver.com>
2023-03-29 15:46:27 +0900

    feat: pair, triple data class 추가

---
+++ lib/presentation/utils/dto/Triple.dart
@@ -0,0 +1,10 @@
+class Triple {
+  Triple(this.first, this.second, this.third);
+
+  final dynamic first;
+  final dynamic second;
+  final dynamic third;
+
+  @override
+  String toString() => 'Triple[$first, $second, $third]';
+}
