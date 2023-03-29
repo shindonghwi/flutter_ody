c5451755036c3d93f4eefe9eb40e162e9ca00377
shindonghwi <ehdgnl8940@naver.com>
2023-03-29 15:46:45 +0900

    feat: splash 화면 제작

--- lib/presentation/features/splash/SplashScreen.dart
+++ lib/presentation/features/splash/SplashScreen.dart
@@ -1,11 +1,29 @@
-import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'dart:async';
 
-class SplashScreen extends HookConsumerWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:odac_flutter_app/presentation/features/onboarding/OnBoardingScreen.dart';
+import 'package:odac_flutter_app/presentation/ui/colors.dart';
+import 'package:odac_flutter_app/presentation/utils/Common.dart';
+
+class SplashScreen extends HookWidget {
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Center(
-      child: Image.asset('assets/imgs/logo_ody.png'),
+  Widget build(BuildContext context) {
+    useEffect(() {
+      Timer(Duration(seconds: 2), () {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (BuildContext context) => OnBoardingScreen(),
+          ),
+        );
+      });
+    }, []);
+
+    return Scaffold(
+      backgroundColor: getColorScheme(context).colorBackground,
+      body: Center(
+        child: Image.asset("assets/imgs/logo_ody.png"),
+      ),
     );
   }
 }
