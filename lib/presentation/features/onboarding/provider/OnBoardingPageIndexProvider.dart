e83e1d5c314284bdbefad4e23992a1d7bde056a5
shindonghwi <ehdgnl8940@naver.com>
2023-04-10 14:57:36 +0900

    fix: pageview provider -> StateProvider.autoDispose 변경

--- lib/presentation/features/onboarding/provider/OnBoardingPageIndexProvider.dart
+++ lib/presentation/features/onboarding/provider/OnBoardingPageIndexProvider.dart
@@ -1,3 +1,3 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final onBoardingCurrentPageIndexProvider = StateProvider<int>((ref) => 0);
+final onBoardingCurrentPageIndexProvider = StateProvider.autoDispose<int>((ref) => 0);
--- lib/presentation/features/onboarding/provider/OnBoardingPageViewController.dart
+++ lib/presentation/features/onboarding/provider/OnBoardingPageViewController.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final onBoardingPageViewControllerProvider = Provider<PageController>(
+final onBoardingPageViewControllerProvider = StateProvider.autoDispose<PageController>(
   (ref) => PageController(
     keepPage: true,
     initialPage: 0,
