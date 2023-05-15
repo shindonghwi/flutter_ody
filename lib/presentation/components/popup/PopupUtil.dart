69add0f834db32eaf29ac46860706bc96999b46b
shindonghwi <ehdgnl8940@naver.com>
2023-05-15 15:03:57 +0900

    feat: 사용약관 팝업 애니메이션 적용

--- lib/presentation/components/popup/PopupUtil.dart
+++ lib/presentation/components/popup/PopupUtil.dart
@@ -7,19 +7,33 @@ class PopupUtil {
     final context = OdyGlobalVariable.naviagatorState.currentContext as BuildContext;
     Future.delayed(Duration.zero, () {
       showDialog(
-          context: context,
-          barrierDismissible: backgroundTouchCloseFlag, // 바깥 영역 터치시 닫을지 여부
-          builder: (BuildContext context) {
-            return const AlertDialog(
-              contentPadding: EdgeInsets.zero,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-              ),
-              content: PopUpAppPolicy(),
-            );
-          });
+        context: context,
+        barrierDismissible: backgroundTouchCloseFlag, // Whether to close when touching the outside area
+        builder: (BuildContext context) {
+          return TweenAnimationBuilder<double>(
+            tween: Tween<double>(begin: 0, end: 1),
+            duration: const Duration(milliseconds: 600),
+            builder: (context, value, child) {
+              return Transform.scale(
+                scale: value,
+                child: Opacity(
+                  opacity: value,
+                  child: const AlertDialog(
+                    contentPadding: EdgeInsets.zero,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    content: PopUpAppPolicy(),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      );
     });
   }
+
 
   static void close(){
     final context = OdyGlobalVariable.naviagatorState.currentContext as BuildContext;
