4a20541e9758df65218dec98498e70ef91ffef27
shindonghwi <ehdgnl8940@naver.com>
2023-06-20 18:29:29 +0900

    feat: 약 추가하기 버튼 추가

--- lib/presentation/features/setting/medication/AddMedicationScreen.dart
+++ lib/presentation/features/setting/medication/AddMedicationScreen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:odac_flutter_app/presentation/components/appbar/IconTitleIconAppBar.dart';
 import 'package:odac_flutter_app/presentation/components/appbar/model/AppBarIcon.dart';
+import 'package:odac_flutter_app/presentation/components/button/fill/FillButton.dart';
+import 'package:odac_flutter_app/presentation/components/button/model/ButtonNotifier.dart';
+import 'package:odac_flutter_app/presentation/components/button/model/ButtonSizeType.dart';
+import 'package:odac_flutter_app/presentation/components/button/model/ButtonState.dart';
 import 'package:odac_flutter_app/presentation/features/setting/medication/widget/MedicationName.dart';
 import 'package:odac_flutter_app/presentation/features/setting/medication/widget/MedicationTime.dart';
 import 'package:odac_flutter_app/presentation/features/setting/medication/widget/MedicationYoil.dart';
@@ -23,12 +28,26 @@ class AddMedicationScreen extends StatelessWidget {
         title: getAppLocalizations(context).add_medication_title,
       ),
       body: Column(
-        children: const [
-          MedicationName(),
-          SizedBox(height: 32),
-          MedicationYoil(),
-          SizedBox(height: 32),
-          MedicationTime()
+        children: [
+          const MedicationName(),
+          const SizedBox(height: 32),
+          const MedicationYoil(),
+          const SizedBox(height: 32),
+          const MedicationTime(),
+          const Expanded(child: SizedBox()),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(40, 0, 40, 58),
+            child: FillButton(
+              text: getAppLocalizations(context).common_complete,
+              type: ButtonSizeType.Normal,
+              onPressed: () {},
+              buttonProvider: StateNotifierProvider<ButtonNotifier, ButtonState>(
+                (_) => ButtonNotifier(
+                  state: ButtonState.Activated,
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
