58f3969de62cf2dde68c2d3ecf19d2928fbcb0a0
shindonghwi <ehdgnl8940@naver.com>
2023-06-22 11:18:33 +0900

    feat: 화면 이동 애니메이션 swipe back 기능 구현 ( ios )

--- lib/presentation/navigation/PageMoveUtil.dart
+++ lib/presentation/navigation/PageMoveUtil.dart
@@ -1,33 +1,48 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:ody_flutter_app/presentation/navigation/Route.dart';
 
-PageRouteBuilder nextSlideScreen(String route, {dynamic parameter}) {
-  return PageRouteBuilder(
-    transitionDuration: const Duration(milliseconds: 250),
-    pageBuilder: (context, animation, secondaryAnimation) =>
-        RoutingScreen.getScreen(route, parameter: parameter),
-    reverseTransitionDuration: const Duration(milliseconds: 250),
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      return SlideTransition(
-        position: Tween(begin: const Offset(1.0, 0.0), end: const Offset(0.0, 0.0))
-            .animate(animation),
-        child: child,
-      );
-    },
-  );
+PageRoute nextSlideScreen(String route, {dynamic parameter}) {
+  if (Platform.isIOS) {
+    return CupertinoPageRoute(
+      builder: (context) => RoutingScreen.getScreen(route, parameter: parameter),
+    );
+  } else {
+    return PageRouteBuilder(
+      transitionDuration: const Duration(milliseconds: 250),
+      pageBuilder: (context, animation, secondaryAnimation) =>
+          RoutingScreen.getScreen(route, parameter: parameter),
+      reverseTransitionDuration: const Duration(milliseconds: 250),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        return SlideTransition(
+          position: Tween(begin: const Offset(1.0, 0.0), end: const Offset(0.0, 0.0))
+              .animate(animation),
+          child: child,
+        );
+      },
+    );
+  }
 }
 
-PageRouteBuilder nextFadeInOutScreen(String route, {dynamic parameter}) {
-  return PageRouteBuilder(
-    transitionDuration: const Duration(milliseconds: 250),
-    pageBuilder: (context, animation, secondaryAnimation) =>
-        RoutingScreen.getScreen(route, parameter: parameter),
-    reverseTransitionDuration: const Duration(milliseconds: 250),
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      return FadeTransition(
-        opacity: animation,
-        child: child,
-      );
-    },
-  );
+
+PageRoute nextFadeInOutScreen(String route, {dynamic parameter}) {
+  if (Platform.isIOS) {
+    return CupertinoPageRoute(
+      builder: (context) => RoutingScreen.getScreen(route, parameter: parameter),
+    );
+  } else {
+    return PageRouteBuilder(
+      transitionDuration: const Duration(milliseconds: 250),
+      pageBuilder: (context, animation, secondaryAnimation) =>
+          RoutingScreen.getScreen(route, parameter: parameter),
+      reverseTransitionDuration: const Duration(milliseconds: 250),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        return FadeTransition(
+          opacity: animation,
+          child: child,
+        );
+      },
+    );
+  }
 }
