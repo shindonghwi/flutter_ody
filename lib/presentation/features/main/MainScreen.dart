9569bd0d7ca2ae60d27b434f894e29ed5df57579
shindonghwi <ehdgnl8940@naver.com>
2023-06-28 17:38:02 +0900

    feat: 달력 확장시 바텀메뉴 dim 처리

--- lib/presentation/features/main/MainScreen.dart
+++ lib/presentation/features/main/MainScreen.dart
@@ -4,6 +4,9 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ody_flutter_app/presentation/features/main/analysis/AnalysisScreen.dart';
 import 'package:ody_flutter_app/presentation/features/main/home/HomeScreen.dart';
 import 'package:ody_flutter_app/presentation/features/main/home/model/CalendarSize.dart';
+import 'package:ody_flutter_app/presentation/features/main/home/notifier/CalendarFormatNotifier.dart';
+import 'package:ody_flutter_app/presentation/features/main/home/notifier/CalendarHeightNotifier.dart';
+import 'package:ody_flutter_app/presentation/features/main/home/notifier/DimNotifier.dart';
 import 'package:ody_flutter_app/presentation/features/main/home/widget/HomeAppBar.dart';
 import 'package:ody_flutter_app/presentation/features/main/home/widget/calendar/CalendarContainerView.dart';
 import 'package:ody_flutter_app/presentation/features/main/home/widget/calendar/CalendarContentView.dart';
@@ -13,6 +16,7 @@ import 'package:ody_flutter_app/presentation/ui/colors.dart';
 import 'package:ody_flutter_app/presentation/ui/typography.dart';
 import 'package:ody_flutter_app/presentation/utils/Common.dart';
 import 'package:ody_flutter_app/presentation/utils/dto/Pair.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class MainScreen extends HookConsumerWidget {
   @override
@@ -48,63 +52,115 @@ class MainScreen extends HookConsumerWidget {
             ),
         ],
       ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border(
-              top: BorderSide(
-                color: getColorScheme(context).neutral50,
-                width: 1,
+      bottomNavigationBar: _BottomNavigationBar(currentIndex: _currentIndex, iconList: _iconList),
+    );
+  }
+}
+
+class _BottomNavigationBar extends HookConsumerWidget {
+  const _BottomNavigationBar({
+    super.key,
+    required int currentIndex,
+    required List<Pair> iconList,
+  })
+      : _currentIndex = currentIndex,
+        _iconList = iconList;
+
+  final int _currentIndex;
+  final List<Pair> _iconList;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    const minAlpha = 0.0;
+    const maxAlpha = 0.3;
+    const duration = 200;
+
+    final isDimOn = ref.watch<bool>(dimProvider);
+    final isDimOnRead = ref.read(dimProvider.notifier);
+    final calendarFormatRead = ref.read(calendarFormatProvider.notifier);
+    final calendarHeightRead = ref.read(calendarHeightProvider.notifier);
+
+    return TweenAnimationBuilder<double>(
+      duration: const Duration(milliseconds: duration),
+      tween: Tween<double>(begin: 0.0, end: isDimOn ? maxAlpha : minAlpha),
+      builder: (_, value, child) {
+        return Stack(
+          children: [
+            Container(
+              color: getColorScheme(context).white,
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(15.0),
+                  topRight: Radius.circular(15.0),
+                ),
+                child: BottomNavigationBar(
+                  type: BottomNavigationBarType.fixed,
+                  backgroundColor: getColorScheme(context).white,
+                  selectedItemColor: getColorScheme(context).colorPrimaryFocus,
+                  unselectedItemColor: getColorScheme(context).neutral50,
+                  currentIndex: _currentIndex,
+                  onTap: (index) =>
+                  ref
+                      .read(mainTabIndexProvider.notifier)
+                      .state = index,
+                  items: _iconList.map((data) {
+                    return BottomNavigationBarItem(
+                      icon: Column(
+                        children: [
+                          SvgPicture.asset(
+                            data.first,
+                            width: 24,
+                            height: 24,
+                            colorFilter: ColorFilter.mode(
+                              getColorScheme(context).neutral50,
+                              BlendMode.srcIn,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 4,
+                          )
+                        ],
+                      ),
+                      activeIcon: Column(
+                        children: [
+                          SvgPicture.asset(
+                            data.first,
+                            width: 24,
+                            height: 24,
+                            colorFilter: ColorFilter.mode(
+                              getColorScheme(context).colorPrimaryFocus,
+                              BlendMode.srcIn,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 4,
+                          )
+                        ],
+                      ),
+                      label: data.second,
+                    );
+                  }).toList(),
+                  selectedLabelStyle: getTextTheme(context).c3b,
+                  unselectedLabelStyle: getTextTheme(context).c3b,
+                ),
               ),
-            )),
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.white,
-          selectedItemColor: getColorScheme(context).colorPrimaryFocus,
-          unselectedItemColor: getColorScheme(context).neutral50,
-          currentIndex: _currentIndex,
-          onTap: (index) => ref.read(mainTabIndexProvider.notifier).state = index,
-          items: _iconList.map((data) {
-            return BottomNavigationBarItem(
-              icon: Column(
-                children: [
-                  SvgPicture.asset(
-                    data.first,
-                    width: 24,
-                    height: 24,
-                    colorFilter: ColorFilter.mode(
-                      getColorScheme(context).neutral50,
-                      BlendMode.srcIn,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  )
-                ],
+            ),
+            if (isDimOn)
+              GestureDetector(
+                onTap: () {
+                  calendarHeightRead.updateHeight(CalendarSize.minHeight(context));
+                  calendarFormatRead.updateFormat(CalendarFormat.week);
+                  isDimOnRead.change(false);
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: 60,
+                  color: getColorScheme(context).black.withOpacity(value),
+                ),
               ),
-              activeIcon: Column(
-                children: [
-                  SvgPicture.asset(
-                    data.first,
-                    width: 24,
-                    height: 24,
-                    colorFilter: ColorFilter.mode(
-                      getColorScheme(context).colorPrimaryFocus,
-                      BlendMode.srcIn,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  )
-                ],
-              ),
-              label: data.second,
-            );
-          }).toList(),
-          selectedLabelStyle: getTextTheme(context).c3b,
-          unselectedLabelStyle: getTextTheme(context).c3b,
-        ),
-      ),
+          ],
+        );
+      },
     );
   }
 }
