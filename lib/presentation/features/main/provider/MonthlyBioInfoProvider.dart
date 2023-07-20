30073e3dd7f559a8519a9edf5992273dd6528bdf
shindonghwi <ehdgnl8940@naver.com>
2023-07-20 15:46:09 +0900

    fix: 달력에 날짜표시기능 수정

--- lib/presentation/features/main/provider/MonthlyBioInfoProvider.dart
+++ lib/presentation/features/main/provider/MonthlyBioInfoProvider.dart
@@ -3,29 +3,20 @@ import 'package:ody_flutter_app/domain/usecases/remote/bio/GetBioHistoryMontlyUs
 import 'package:ody_flutter_app/presentation/models/UiState.dart';
 import 'package:riverpod/riverpod.dart';
 
-final monthlyBioInfoProvider = StateNotifierProvider<MonthlyBioInfoNotifier, UIState<List<String>?>>(
+final monthlyBioInfoProvider = StateNotifierProvider<MonthlyBioInfoNotifier, UIState<List<String>>>(
   (_) => MonthlyBioInfoNotifier(),
 );
 
-class MonthlyBioInfoNotifier extends StateNotifier<UIState<List<String>?>> {
+class MonthlyBioInfoNotifier extends StateNotifier<UIState<List<String>>> {
   MonthlyBioInfoNotifier() : super(Idle());
 
-  Future<List<String>?> requestBioInfo(int year, int month, int day) async {
+  void requestBioInfo(int year, int month, int day) async {
     state = Loading();
     final res = await GetIt.instance.get<GetBioHistoryMontlyUseCase>().call(year: year, month: month);
     if (res.status == 200) {
-      _updateBioInfo(res.list);
+      state = Success(res.list ?? []);
     } else {
       state = Failure(res.message);
-    }
-    return res.list;
-  }
-
-  void _updateBioInfo(List<String>? data) {
-    if (data != null) {
-      state = Success(data);
-    } else {
-      state = Success(null);
     }
   }
 
